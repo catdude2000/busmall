8432f4209db7a6d03f0eeb13218c69ecaa19f9d0
catdude2000 <michael.g.pace@gmail.com>
2021-08-09 20:07:27 -0500

    a little work

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 'use strict';
+console.log('js file is connected');
 
 const bag = {
-    name = r2bag
-    
+    name = 'r2bag',
+    filepath = url(https://raw.githubusercontent.com/DeltaVCode/cedarrapids-code-201d10/main/class-11/lab/assets/bag.jpg),
+    timesshown = 0,
 }
